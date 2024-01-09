6b2cce389b6a00273a09a2d5a40e6716aeb09187
Ajay Irkal <Ajayirkal.mercy@gmail.com>
2024-01-09 11:33:40 +0530

    hhhh

---
+++ runClient.py
@@ -0,0 +1,12 @@
+import subprocess
+import os
+
+# Define the path and the command
+path = "E:\\codes\\DM Dashboard\\client"
+command = "npm run dev"
+
+# Change the current working directory
+os.chdir(path)
+
+# Run the command
+subprocess.run(command, shell=True)
